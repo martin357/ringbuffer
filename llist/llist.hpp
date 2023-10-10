dd46820188c85bf41b1653f760166f9c08125bb0
Martin Kopecky <martin.kopecky@prusa3d.cz>
2023-10-10 17:22:31 +0200

    Init: Linked-list with tests

---
+++ llist/llist.hpp
@@ -0,0 +1,94 @@
+#pragma once
+
+#include <cassert>
+
+template<class T>
+class LList {
+    struct Node {
+        T value;
+        Node * prev{nullptr};
+        Node * next{nullptr};
+    };
+    Node * head{nullptr};
+    Node * tail{nullptr};
+public:
+    class iterator {
+        Node * node{nullptr};
+        iterator(Node * node) { this->node = node; }
+        friend class LList;
+    public:
+        inline T & operator*() { assert(node); return node->value; }
+        inline iterator & operator++() { assert(node); node = node->next; return *this; } // pre
+        inline iterator & operator--() { assert(node); node = node->prev; return *this; } // pre
+        inline iterator operator++(int) { 
+            assert(node); 
+            auto tmp = iterator(*this); 
+            node = node->next; 
+            return tmp; 
+        } // post
+        inline iterator operator--(int) {
+            assert(node);
+            auto tmp = iterator(*this); 
+            node = node->prev; 
+            return tmp; 
+        } // post
+        inline bool operator==(const iterator & other) const { return this->node == other.node; }
+        inline bool operator!=(const iterator & other) const { return !(*this == other); }
+    };
+
+    class const_iterator : public iterator {
+    public:
+        inline const T & operator*() { assert(this->node); return this->node->value; }
+    };
+
+    iterator begin() const { return iterator(head); }
+    iterator end() const { return iterator(nullptr); }
+    const_iterator const_begin() const { return const_iterator(head); }
+    const_iterator const_end() const { return const_iterator(nullptr); }
+
+    void push_front(const T & item) {
+        Node * node = new Node{item, nullptr, nullptr};
+        if( ! head ) { head = tail = node; }
+        else { head->prev = node; node->next = head; head = node; }
+        assert(head == node);
+    }
+
+    void push_back(const T & item) {
+        Node * node = new Node{item, nullptr, nullptr};
+        if( ! tail ) { head = tail = node; }
+        else { tail->next = node; node->prev = tail; tail = node; }
+        assert(tail == node);
+    }
+        
+    void pop_front() {
+        assert(head);
+        Node * tmp = head;
+        head = head->next;
+        if( !head ) tail = nullptr;
+        delete tmp;
+    }
+
+    void pop_back() {
+        assert(tail);
+        Node * tmp = tail;
+        tail = tail->prev;
+        if( !tail ) head = nullptr;
+        delete tmp;
+    }
+
+    bool empty() {
+        assert( (!head) == (!tail) );
+        return head == nullptr;
+    }
+
+    const T & front() const { return head->value; }
+    const T & back() const { return tail->value; }
+
+    void clear() {
+        while(!empty()) pop_back();
+    }
+
+    ~LList() {
+        clear();
+    }
+};
---
+++ tests/tests.cpp
@@ -0,0 +1,107 @@
+#include <gtest/gtest.h>
+#include "llist.hpp"
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
+
+TEST(push_back_pop_back_behaviour, BasicAssertions) {
+    LList<int> list;
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+    EXPECT_EQ(list.front(), 1);
+    EXPECT_EQ(list.back(), 3);
+    EXPECT_EQ(list.empty(), false);
+    list.pop_back();
+    EXPECT_EQ(list.front(), 1);
+    EXPECT_EQ(list.back(), 2);
+    EXPECT_EQ(list.empty(), false);
+    list.pop_back();
+    EXPECT_EQ(list.front(), 1);
+    EXPECT_EQ(list.back(), 1);
+    EXPECT_EQ(list.empty(), false);
+    list.pop_back();
+    EXPECT_EQ(list.empty(), true);
+}
+
+TEST(push_front_pop_front_behaviour, BasicAssertions) {
+    LList<int> list;
+    list.push_front(1);
+    list.push_front(2);
+    list.push_front(3);
+    EXPECT_EQ(list.front(), 3);
+    EXPECT_EQ(list.back(), 1);
+    EXPECT_EQ(list.empty(), false);
+    list.pop_front();
+    EXPECT_EQ(list.front(), 2);
+    EXPECT_EQ(list.back(), 1);
+    EXPECT_EQ(list.empty(), false);
+    list.pop_front();
+    EXPECT_EQ(list.front(), 1);
+    EXPECT_EQ(list.back(), 1);
+    EXPECT_EQ(list.empty(), false);
+    list.pop_front();
+    EXPECT_EQ(list.empty(), true);
+}
+
+TEST(iterators, BasicAssertions) {
+    LList<int> list;
+    for(int i = 0; i < 10; ++i) {
+        list.push_back(i);
+        EXPECT_EQ(list.front(), 0);
+        EXPECT_EQ(list.back(), i);
+        int j = 0;
+        for(auto it = list.begin(); it != list.end(); ++it) {
+            EXPECT_EQ(*it, j++);
+        }
+        j = 0;
+        for(auto it = list.begin(); it != list.end(); it++) {
+            EXPECT_EQ(*it, j++);
+        }
+    }
+}
+
+TEST(const_iterators, BasicAssertions) {
+    LList<int> list;
+    for(int i = 0; i < 10; ++i) {
+        list.push_back(i);
+        EXPECT_EQ(list.front(), 0);
+        EXPECT_EQ(list.back(), i);
+        int j = 0;
+        for(auto it = list.const_begin(); it != list.const_end(); ++it) {
+            EXPECT_EQ(*it, j++);
+        }
+        j = 0;
+        for(auto it = list.const_begin(); it != list.const_end(); it++) {
+            EXPECT_EQ(*it, j++);
+        }
+    }
+}
+
+struct LeakingCanary {
+    static int i;
+    LeakingCanary() {
+        i++;
+    }
+    ~LeakingCanary() {
+        i--;
+    }
+    static bool is_leaking() { return 0 == i; }
+};
+int LeakingCanary::i = 0;
+
+TEST(destructor, BasicAssertions) {
+    {
+        LList<LeakingCanary> list;
+        for(int i = 0; i < 10; i++) {
+            list.push_back(LeakingCanary());
+        }
+    }
+    EXPECT_EQ(false, LeakingCanary::is_leaking());
+
+}
